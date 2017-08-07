ef2d675e655eb9d095021911e52458bbfcbfe470
YucelT94 <yucelterlemezuglu@gmail.com>
2017-08-07 16:24:27 +0300

    news added

--- src/screens/news/Dunya.js
+++ src/screens/news/Dunya.js
@@ -1,10 +1,47 @@
 import React, {Component} from 'react';
-import {Tile, List, ListItem, Button, Text} from 'react-native-elements';
+import {StyleSheet, ActivityIndicator, ListView, Text, View} from 'react-native';
 
 class Dunya extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true
+        }
+    }
+
+    componentDidMount() {
+        return fetch('https://esgazete-api.herokuapp.com/esgazete/world')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                let ds = new ListView.DataSource({rowHasChanged: (r1, r2, r3) => r1 !== r2 !== r3});
+                this.setState({
+                    isLoading: false,
+                    dataSource: ds.cloneWithRows(responseJson),
+                }, function () {
+                    // do something with new state
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
     render() {
+        if (this.state.isLoading) {
+            return (
+                <View style={{flex: 1, paddingTop: 20}}>
+                    <ActivityIndicator/>
+                </View>
+            );
+        }
+
         return (
-            <Text> Dunya </Text>
+            <View style={{flex: 1, paddingTop: 20}}>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={(rowData) => <Text>{rowData.title}, {rowData.url}, {rowData.picUrl}</Text>}
+                />
+            </View>
         );
     }
 }
