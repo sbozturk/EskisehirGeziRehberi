ab25bc054e81bd349b92ac8e18f64265583dd23e
Satuk Buğrahan Öztürk <sbozturk1907@gmail.com>
2017-08-10 13:56:53 +0300

    button size fix

--- src/screens/PhoneNumbers.js
+++ src/screens/PhoneNumbers.js
@@ -10,38 +10,38 @@ class PhoneNumbers extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <TouchableOpacity onPress={() => Communications.phonecall('112', true)}>
-                    <View style={styles.holder}>
+                <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('112', true)}>
+                    <View>
                         <Text style={styles.text}>Ambulans</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => Communications.phonecall('110', true)}>
-                    <View style={styles.holder}>
+                <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('110', true)}>
+                    <View>
                         <Text style={styles.text}>İtfaiye</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => Communications.phonecall('155', true)}>
-                    <View style={styles.holder}>
+                <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('155', true)}>
+                    <View>
                         <Text style={styles.text}>Polis</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => Communications.phonecall('156', true)}>
-                    <View style={styles.holder}>
+                <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('156', true)}>
+                    <View>
                         <Text style={styles.text}>Jandarma</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => Communications.phonecall('185', true)}>
-                    <View style={styles.holder}>
+                <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('185', true)}>
+                    <View>
                         <Text style={styles.text}>Su Arıza</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => Communications.phonecall('186', true)}>
-                    <View style={styles.holder}>
+                <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('186', true)}>
+                    <View>
                         <Text style={styles.text}>Elektrik Arıza</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => Communications.phonecall('187', true)}>
-                    <View style={styles.holder}>
+                <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('187', true)}>
+                    <View>
                         <Text style={styles.text}>Gaz Arıza</Text>
                     </View>
                 </TouchableOpacity>
@@ -55,12 +55,15 @@ var styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         backgroundColor: '#fff',
+        justifyContent: 'center',
     },
     holder: {
+        justifyContent: 'center',
+        borderRadius: 100 / 10,
         backgroundColor: '#05265b',
-        marginTop: 15,
-        height: '11%',
-        width: 300,
+        marginBottom: 20,
+        height: '10%',
+        width: '75%',
     },
     text: {
         textAlign: 'center',
