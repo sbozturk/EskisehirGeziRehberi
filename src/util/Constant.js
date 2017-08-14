d27f2de9539e8a15414fd66c76b389a81a8c5c5a
YucelT94 <yucelterlemezuglu@gmail.com>
2017-08-14 11:03:52 +0300

    Constant.js added

---
+++ src/util/Constant.js
@@ -0,0 +1,12 @@
+class Constant {
+    static REQUEST_URL_DUNYA = 'https://esgazete-api.herokuapp.com/esgazete/world';
+    static REQUEST_URL_EGLENCE = 'https://esgazete-api.herokuapp.com/esgazete/fun';
+    static REQUEST_URL_EKONOMI = 'https://esgazete-api.herokuapp.com/esgazete/economy';
+    static REQUEST_URL_GUNDEM = 'https://esgazete-api.herokuapp.com/esgazete/agenda';
+    static REQUEST_URL_MAGAZIN = 'https://esgazete-api.herokuapp.com/esgazete/magazin';
+    static REQUEST_URL_SIYASET = 'https://esgazete-api.herokuapp.com/esgazete/politics';
+    static REQUEST_URL_SPOR = 'https://esgazete-api.herokuapp.com/esgazete/sport';
+    static REQUEST_URL_TEKNOLOJI = 'https://esgazete-api.herokuapp.com/esgazete/technology';
+}
+
+export default Constant;
