54a12ac606882c4da596ceb63a6edef25403dd9a
YucelT94 <yucelterlemezuglu@gmail.com>
2017-08-14 16:15:55 +0300

    PhoneNumbeers.js styles transport Styles.js

--- src/screens/PhoneNumbers.js
+++ src/screens/PhoneNumbers.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, TouchableOpacity, Text, View, Image} from 'react-native';
+import {TouchableOpacity, Text, View, Image} from 'react-native';
 import Communications from 'react-native-communications';
+import Styles from "../util/Styles";
 
-class PhoneNumbers extends React.Component {
+class PhoneNumbers extends Component {
     static navigationOptions = {
         title: 'Acil Numaralar',
     };
@@ -13,70 +14,46 @@ class PhoneNumbers extends React.Component {
                 style={{flex: 1, resizeMode: 'stretch', width: '100%'}}
                 source={require('../assets/background.png')}
             >
-                <View style={styles.container}>
-                    <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('112', true)}>
+                <View style={Styles.stylePharmancyDetail.container}>
+                    <TouchableOpacity style={Styles.stylePharmancyDetail.holder} onPress={() => Communications.phonecall('112', true)}>
                         <View>
-                            <Text style={styles.text}>Ambulans</Text>
+                            <Text style={Styles.stylePharmancyDetail.text}>Ambulans</Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('110', true)}>
+                    <TouchableOpacity style={Styles.stylePharmancyDetail.holder} onPress={() => Communications.phonecall('110', true)}>
                         <View>
-                            <Text style={styles.text}>İtfaiye</Text>
+                            <Text style={Styles.stylePharmancyDetail.text}>İtfaiye</Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('155', true)}>
+                    <TouchableOpacity style={Styles.stylePharmancyDetail.holder} onPress={() => Communications.phonecall('155', true)}>
                         <View>
-                            <Text style={styles.text}>Polis</Text>
+                            <Text style={Styles.stylePharmancyDetail.text}>Polis</Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('156', true)}>
+                    <TouchableOpacity style={Styles.stylePharmancyDetail.holder} onPress={() => Communications.phonecall('156', true)}>
                         <View>
-                            <Text style={styles.text}>Jandarma</Text>
+                            <Text style={Styles.stylePharmancyDetail.text}>Jandarma</Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('185', true)}>
+                    <TouchableOpacity style={Styles.stylePharmancyDetail.holder} onPress={() => Communications.phonecall('185', true)}>
                         <View>
-                            <Text style={styles.text}>Su Arıza</Text>
+                            <Text style={Styles.stylePharmancyDetail.text}>Su Arıza</Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('186', true)}>
+                    <TouchableOpacity style={Styles.stylePharmancyDetail.holder} onPress={() => Communications.phonecall('186', true)}>
                         <View>
-                            <Text style={styles.text}>Elektrik Arıza</Text>
+                            <Text style={Styles.stylePharmancyDetail.text}>Elektrik Arıza</Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.holder} onPress={() => Communications.phonecall('187', true)}>
+                    <TouchableOpacity style={Styles.stylePharmancyDetail.holder} onPress={() => Communications.phonecall('187', true)}>
                         <View>
-                            <Text style={styles.text}>Gaz Arıza</Text>
+                            <Text style={Styles.stylePharmancyDetail.text}>Gaz Arıza</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
-
             </Image>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: 'transparent',
-        justifyContent: 'center',
-    },
-    holder: {
-        justifyContent: 'center',
-        backgroundColor: 'transparent',
-        height: '14.3%',
-        width: '100%',
-        borderColor: '#fff',
-        borderWidth: 2,
-    },
-    text: {
-        textAlign: 'center',
-        fontSize: 32,
-        color: '#fff',
-    },
-});
-
 
 export default PhoneNumbers;
