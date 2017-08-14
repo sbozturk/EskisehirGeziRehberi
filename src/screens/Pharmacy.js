7975155bb5c98a59534d5c6d14886d6b707cd1ff
YucelT94 <yucelterlemezuglu@gmail.com>
2017-08-14 16:15:33 +0300

    Pharmancy.js styles transport Styles.js

--- src/screens/Pharmacy.js
+++ src/screens/Pharmacy.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    Image
-} from 'react-native';
+import {Text, View, TouchableOpacity, Image} from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import Odunpazari from './pharmacy/Odunpazari';
 import Tepebasi from './pharmacy/Tepebasi';
+import Styles from "../util/Styles";
 
 class Pharmacy extends React.Component {
     static navigationOptions = {
@@ -19,26 +14,25 @@ class Pharmacy extends React.Component {
         const {navigate} = this.props.navigation;
 
         return (
-
             <Image
                 style={{flex: 1, resizeMode: 'stretch', width: '100%'}}
                 source={require('../assets/background.png')}
             >
 
-                <View style={columnStyle.container}>
+                <View style={Styles.stylePharmancy.containerColumn}>
 
                     <TouchableOpacity
-                        style={buttonStyle.button}
+                        style={Styles.stylePharmancy.button}
                         onPress={() => navigate('Tepebasi')}
                     >
-                        <Text style={buttonStyle.buttonText}>Tepebaşı</Text>
+                        <Text style={Styles.stylePharmancy.buttonText}>Tepebaşı</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity
-                        style={buttonStyle.button}
+                        style={Styles.stylePharmancy.button}
                         onPress={() => navigate('Odunpazari')}
                     >
-                        <Text style={buttonStyle.buttonText}>Odunpazarı</Text>
+                        <Text style={Styles.stylePharmancy.buttonText}>Odunpazarı</Text>
                     </TouchableOpacity>
                 </View>
 
@@ -56,33 +50,5 @@ const App = StackNavigator({
         headerMode: 'none',
     }
 );
-
-const columnStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-});
-
-const buttonStyle = StyleSheet.create({
-
-    button: {
-        backgroundColor: 'transparent',
-        height: '50%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderColor: '#fff',
-        borderWidth: 2,
-    },
-
-    buttonText: {
-        textAlign: 'center',
-        fontSize: 56,
-        color: 'white',
-    },
-});
-
 
 export default App;
