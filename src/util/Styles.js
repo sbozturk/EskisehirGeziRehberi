14b7ce64e10fdaf645f612909ba6ac6a519d675b
YucelT94 <yucelterlemezuglu@gmail.com>
2017-08-14 15:36:12 +0300

    weather style added

--- src/util/Styles.js
+++ src/util/Styles.js
@@ -22,40 +22,8 @@ class Styles {
             padding: 8,
         },
     });
-    static stylePharmancyDetail = StyleSheet.create({
-        container: {
-            alignItems: 'center',
-            justifyContent: 'center',
-            flex: 1,
-            backgroundColor: '#fff',
-            borderColor: '#fff',
-            borderWidth: 2,
-            marginTop: 10,
-            marginLeft: 20,
-            marginRight: 20
-        },
 
-        title: {
-            fontSize: 16,
-            marginBottom: 8,
-            marginTop: 10,
-            marginRight: 10,
-            marginLeft: 10,
-            fontWeight: 'bold',
-        },
-
-        text: {
-            marginRight: 10,
-            marginLeft: 10,
-        },
-
-        textTelephone: {
-            marginRight: 10,
-            marginLeft: 10,
-            marginTop: 10,
-            marginBottom: 10
-        },
-
+    static styleWeather = StyleSheet.create({
         centering: {
             alignItems: 'center',
             justifyContent: 'center',
