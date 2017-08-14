a7f0fc0701822e30ea4ca7ea1ef24318477fb9c9
YucelT94 <yucelterlemezuglu@gmail.com>
2017-08-14 16:16:15 +0300

    Tramvat.js styles transport Styles.js

--- src/screens/Tramvay.js
+++ src/screens/Tramvay.js
@@ -1,12 +1,7 @@
 import React, {Component} from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    Image,
-} from 'react-native';
+import {Text, View, TouchableOpacity, Image} from 'react-native';
 import {StackNavigator} from 'react-navigation';
+import Styles from "../util/Styles";
 
 let imgRequire;
 
@@ -23,142 +18,141 @@ class Tramvay extends React.Component {
                 style={{flex: 1, resizeMode: 'stretch', width: '100%'}}
                 source={require('../assets/background.png')}
             >
-                <View style={rowStyle.container}>
-                    <View style={columnStyle.container}>
+                <View style={Styles.styleTramvay.containerRow}>
+                    <View style={Styles.styleTramvay.containerColumn}>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/a.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>SSK OTOGAR</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>SSK OTOGAR</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/c.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>OSMANGAZİ SSK</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>OSMANGAZİ SSK</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/e.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>OSMANGAZİ OTOGAR</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>OSMANGAZİ OTOGAR</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/g.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>EMEK/71 EVLER</Text>
-                            <Text style={buttonStyle.buttonText}>OPERA</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>EMEK/71 EVLER</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>OPERA</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/i.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>SSK BATIKENT</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>SSK BATIKENT</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/k.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>SSK ÇAMLICA</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>SSK ÇAMLICA</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/m.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>OGÜ ÇANKAYA</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>OGÜ ÇANKAYA</Text>
                         </TouchableOpacity>
                     </View>
 
-                    <View style={columnStyle.container}>
+                    <View style={Styles.styleTramvay.containerColumn}>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/b.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>OTOGAR SSK</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>OTOGAR SSK</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/d.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>SSK OSMANGAZİ</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>SSK OSMANGAZİ</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/f.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>OTOGAR OSMANGAZİ</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>OTOGAR OSMANGAZİ</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/h.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>OPERA</Text>
-                            <Text style={buttonStyle.buttonText}>EMEK/71 EVLER</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>OPERA</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>EMEK/71 EVLER</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/j.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>BATIKENT SSK</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>BATIKENT SSK</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/l.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>ÇAMLICA SSK</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>ÇAMLICA SSK</Text>
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={buttonStyle.button}
+                            style={Styles.styleTramvay.button}
                             onPress={() => {
                                 imgRequire = require('../assets/tramvay/n.jpg');
                                 navigate('TramvaySaat')
                             }}
                         >
-                            <Text style={buttonStyle.buttonText}>ÇANKAYA OGÜ</Text>
+                            <Text style={Styles.styleTramvay.buttonText}>ÇANKAYA OGÜ</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
-
             </Image>
         );
     }
@@ -171,9 +165,9 @@ class TramvaySaat extends Component {
 
     render() {
         return (
-            <View style={imgStyle.container}>
+            <View style={Styles.styleTramvay.containerImg}>
                 <Image
-                    style={imgStyle.img}
+                    style={Styles.styleTramvay.img}
                     source={imgRequire}
                 />
             </View>
@@ -189,54 +183,5 @@ const App = StackNavigator({
         headerMode: 'none',
     }
 );
-
-const imgStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    img: {
-        flex: 1,
-        resizeMode: 'stretch',
-        width: '100%',
-    },
-});
-
-const rowStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: 'transparent'
-    },
-});
-
-const columnStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-
-    },
-});
-
-const buttonStyle = StyleSheet.create({
-    button: {
-        backgroundColor: 'transparent',
-        height: '14.4%',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderColor: '#fff',
-        borderWidth: 2,
-    },
-
-    buttonText: {
-        textAlign: 'center',
-        fontSize: 18,
-        color: 'white',
-        fontWeight: 'bold'
-    },
-});
-
 
 export default App;
