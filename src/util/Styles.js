f38085ca0a05c161ed10fb235967bb06f5a661d6
YucelT94 <yucelterlemezuglu@gmail.com>
2017-08-14 15:40:51 +0300

    styles edited

--- src/util/Styles.js
+++ src/util/Styles.js
@@ -23,6 +23,47 @@ class Styles {
         },
     });
 
+    static stylePharmancyDetail = StyleSheet.create({
+        container: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            flex: 1,
+            backgroundColor: '#fff',
+            borderColor: '#fff',
+            borderWidth: 2,
+            marginTop: 10,
+            marginLeft: 20,
+            marginRight: 20
+        },
+
+        title: {
+            fontSize: 16,
+            marginBottom: 8,
+            marginTop: 10,
+            marginRight: 10,
+            marginLeft: 10,
+            fontWeight: 'bold',
+        },
+
+        text: {
+            marginRight: 10,
+            marginLeft: 10,
+        },
+
+        textTelephone: {
+            marginRight: 10,
+            marginLeft: 10,
+            marginTop: 10,
+            marginBottom: 10
+        },
+
+        centering: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 8,
+        },
+    });
+
     static styleWeather = StyleSheet.create({
         centering: {
             alignItems: 'center',
